8cee7342085f742663393baebc1db0afdcf883cd
Andrey Gruzdev <agruzdev1@gmail.com>
2024-01-29 23:25:14 +0100

    fix parity-secp256k1

--- contract/build.sh
+++ contract/build.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-echo ">> Building contract"
+export RUSTFLAGS='-C link-arg=-s'
 
 rustup target add wasm32-unknown-unknown
 cargo build --all --target wasm32-unknown-unknown --release
